2654f14b7d948de20ee8bddcc5f1af5be032d082
JoelMuteba <mutebakandejoel@hotmail.fr>
2023-03-28 19:36:55 -0400

    Creation de la classe Etudiant

---
+++ Etudiant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationEtudiant
+{
+    internal class Etudiant
+    {
+        public int NumeroEtudiant { get; set; }
+        
+
+        public string NomEtudiant { get; set; }
+
+        public string PrenomEtudiant { get; set; }
+
+
+        public Etudiant(int numeroEtudiant, string nomEtudiant, string prenomEtudiant)
+        {
+            this.NumeroEtudiant = numeroEtudiant;
+            this.NomEtudiant = nomEtudiant;
+            this.PrenomEtudiant = prenomEtudiant;
+        }
+    }
+}
