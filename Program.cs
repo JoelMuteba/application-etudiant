252bb1b7e2eaea3bcbcf6073c42d293464588f2c
JoelMuteba <mutebakandejoel@hotmail.fr>
2023-03-28 19:09:16 -0400

    creation projet c#

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationEtudiant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mon application!");
+        }
+    }
+}
